3f07a9d20e1411a25ea77ab39c0bf39e25a2d24b
Md Rashed Ali <111342025+deve-rashed@users.noreply.github.com>
2023-02-07 09:46:57 +0600

    Uploading HTML, CSS and Javascript Files

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+function rollDice() {
+    
+    var randomNumberPlOne = Math.floor( (Math.random() * 6) + 1 );
+    var diceImgForPlOne = "images/dice" +randomNumberPlOne +".png"
+    
+    var randomNumberPlTwo = Math.floor( (Math.random() * 6) + 1 );
+    var diceImgForPlTwo = "images/dice" +randomNumberPlTwo +".png"
+
+    document.querySelector(".img1").setAttribute("src", diceImgForPlOne);
+    document.querySelector(".img2").setAttribute("src", diceImgForPlTwo);
+
+if (randomNumberPlOne > randomNumberPlTwo ) {
+
+    document.querySelector("h1").textContent = "Player One Win 🏆"
+
+} else if  (randomNumberPlOne === randomNumberPlTwo ) {
+    document.querySelector("h1").textContent = "It's A Tie 💯"
+} else {
+    document.querySelector("h1").textContent = "Player Two Win 🏆"
+}
+  }
+
+  window.onbeforeunload = function() {
+    rollDice();
+  };
+  
+
+
+  window.onload = function() {
+    rollDice();
+  };
+  
